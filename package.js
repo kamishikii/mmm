656b60ce471e94a59ed1675f1e51e3c707c87a5d
kamishiki <ggragra37@gmail.com>
2026-09-09 00:52:46 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "my-music",
+  "version": "1.0.0",
+  "dependencies": {
+    "@capacitor/core": "^6.2.0",
+    "@capacitor/android": "^6.2.0"
+  },
+  "devDependencies": {
+    "@capacitor/cli": "^6.2.0"
+  }
+}
